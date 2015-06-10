1a2f38ffa518d344176a4a88995e20bb4f73ff64
ghaptonstall <ghaptonstall@gmail.com>
2015-06-10 14:13:36 -0700

    --add base guerilla-ui js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,21 @@ var App = require('app');
 
 $(function(){
 
+  $.GUI().create('myApp', function(GUI){
+
+      return {
+          load:function(){
+              GUI.log('hello world :: ', GUI);
+          },
+          unload:function(){
+          
+          }
+      }
+  
+  });
+
+  $.GUI().run();
+
   App.load();
 
 });
