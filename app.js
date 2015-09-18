d65f922b793dbfe5da44d558b1d721f2821d7927
Garrett Haptonstall <ghaptonstall@gmail.com>
2015-09-18 14:31:59 -0700

    --add new guerrilla.js file paths to main layout for using GUI framework. --remove dead code from main app.js file.

--- app.js
+++ app.js
@@ -13,13 +13,6 @@ app.use(express.static(__dirname + '/public'));
 
 routes.addRoutes(app);
 
-/*
-app.get('/', function (req, res) {
-    // res.send('Hello World!');
-    res.render('home', {});
-});
-*/
-
 var server = app.listen(4000, function () {
     var host = server.address().address;
     var port = server.address().port;
