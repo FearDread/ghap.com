6e0f417bc21727ca6fa91a467a9097e5582470d6
Garrett Haptonstall <ghaptonstall@gmail.com>
2015-10-20 17:18:23 -0700

    --update package.json with latest dependencies and update server app logic.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 /* GHAP.com */
-var server, host, port, fs, path, express, routes, 
-    less, utapi, app, pub, router, bodyParser, mongo;
+var server, host, port, fs, path, express, routes, mongoStr, passport, less, utapi, app, pub, bodyParser, mongo;
 
 /* MODS */
 fs = require('fs');
@@ -9,18 +8,20 @@ express = require('express');
 routes = require('./routes');
 less = require('less-middleware');
 mongo = require('mongoose');
+passport = require('passport');
 bodyParser = require('body-parser');
 
 /* APIs */
 utapi = require('./routes/api/ultimate_totals');
 
+/* Tools */
+utils = require('./src/utils');
+config = require('./src/config');
+require('./src/passport')(passport);
+
 /* APP */
 app = express();
 pub = __dirname;
-router = express.Router();
-utils = require('./utils');
-
-mongo.connect('mongodb://localhost/ultimate_totals_db');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -36,11 +37,15 @@ app.use(express.static(path.join(pub, '/public')));
 
 routes.add(app);
 
-utapi.add(app);
+utapi.add(app, passport);
 
-server = app.listen(4000, function () {
-    host = server.address().address;
-    port = server.address().port;
+mongo.connect(config.url, function () {
+  console.log('MongoDB connected at ' + config.url);
+  
+  server = app.listen(4000, function () {
+      host = server.address().address;
+      port = server.address().port;
 
-    console.log('GHAP.com listening at http://%s:%s', host, port);
+      console.log('GHAP.com listening at http://%s:%s', host, port);
+  });
 });
