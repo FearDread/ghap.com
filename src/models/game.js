f966fa0db4725b31fe262fea84acfa10ff12a449
Garrett Haptonstall <ghaptonstall@gmail.com>
2015-11-20 10:37:51 -0800

    update game model with needed db fields and export call, remove un needed account model.

--- src/models/game.js
+++ src/models/game.js
@@ -4,6 +4,17 @@ mongo = require('mongoose');
 Schema = mongo.Schema;
 
 gameSchema = new Schema({
-
-
+    name: String,
+    game_id: {type: Number, required: true},
+    meta: {
+        season: String,
+        leauge: String,
+        teams: {
+            home: String,
+            away: String
+        }
+    }
 });
+
+// export user model
+module.exports = mongo.model('Game', gameSchema);
